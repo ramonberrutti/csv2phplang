f9f865d01c1f965fbc1b581ac0201499ff7e41fa
Ramón Berrutti <ramonberrutti@gmail.com>
2017-09-29 11:50:57 -0300

    first version

---
+++ csv2phplang.php
@@ -0,0 +1,32 @@
+<?php
+
+if($argc < 2 )
+{
+    exit( "Usage: ". $argv[0] . " <file.csv>\n" );
+}
+
+$lang_num = 1;
+if( $argv[1] == '-es' )
+	$lang_num = 2;
+
+
+$empty_space = false;
+if (($handle = fopen($argv[$argc-1], "r")) !== false) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+		$num = count($data);	
+		if( $num < 2 || strlen($data[0]) == 0 ) {
+			if( $empty_space == true ) {
+				echo "\n";
+				$empty_space = false;
+			}
+			continue;
+		}
+		$empty_space = true;		
+
+		echo "\$lang['" . $data[0] . "'] = '" . $data[$lang_num] . "';\n";
+    }
+    fclose($handle);
+} else {
+	echo "fail to open file";
+}
+?>
